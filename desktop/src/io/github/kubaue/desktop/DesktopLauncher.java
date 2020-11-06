ede5987e1492a1e1112b5cf134a04454dd09ae40
Jakub Żmuda <goodguykuba@gmail.com>
2020-11-06 18:35:40 +0100

    initial commit

---
+++ desktop/src/io/github/kubaue/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.kubaue.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.kubaue.RaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RaceGame(), config);
+	}
+}
