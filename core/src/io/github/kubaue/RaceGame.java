4b6f913e1dd7363da805570c540d1a117c2fdcb9
Jakub Żmuda <goodguykuba@gmail.com>
2020-11-08 12:21:47 +0100

    pulled example game

--- core/src/io/github/kubaue/RaceGame.java
+++ core/src/io/github/kubaue/RaceGame.java
@@ -1,33 +1,28 @@
 package io.github.kubaue;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class RaceGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class RaceGame extends Game {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    SpriteBatch batch;
+    BitmapFont font;
+
+    public void create() {
+        batch = new SpriteBatch();
+        // Use LibGDX's default Arial font.
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    public void render() {
+        super.render(); // important!
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
 }
--- desktop/src/io/github/kubaue/desktop/DesktopLauncher.java
+++ desktop/src/io/github/kubaue/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import io.github.kubaue.RaceGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Super racer 9000";
+		config.width = 1920;
+		config.height = 1080;
+
 		new LwjglApplication(new RaceGame(), config);
 	}
 }
